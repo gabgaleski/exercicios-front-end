5a2ad43da22f5c1a5fe720be388aaaad8e20e268
gabgaleski <galeski35@gmail.com>
2023-02-08 19:50:02 -0300

    Cria o ambiente e realiza o formulario com a biblioteca

---
+++ 01-introducao-frond-js-assincrono/dia-01-ambiente-de-desenvolvimento/src/main.js
@@ -0,0 +1,18 @@
+import validator from 'validator';
+
+const btn = document.getElementById('btn-verify');
+const text = document.getElementById('text-input');
+const saida = document.getElementById('text-output');
+const select = document.getElementById('selector');
+
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const objVerify = {
+    email: validator.isEmail(text.value),
+    cpf: validator.isTaxID(text.value, 'pt-BR'),
+    hexColor: validator.isHexColor(text.value),
+    url: validator.isURL(text.value),
+    fone: validator.isMobilePhone(text.value, 'pt-BR'),
+}
+  saida.innerHTML = `A validação retornou: ${objVerify[select.value]}`;
+});
