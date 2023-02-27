e941646e47ff73187cc42933ea9215039a5fa5df
gabgaleski <galeski35@gmail.com>
2023-02-27 15:55:51 -0300

    Realiza exercicio proposto

---
+++ 02-introducao-react/dia-01-iniciando-react/exercise-01/exercise-01/src/About.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function aboutMe() {
+    return (
+        <main>
+          <section>
+            <h1>Gabriel Galeski</h1>
+            <p>Sou um desenvolvedor de 24 anos apaixonado pela tecnologia.</p>
+          </section>
+          <section>
+            <h2> Minhas Habilidades:</h2>
+            <ul>
+                <li>Javascript</li>
+                <li>HTML Semantico</li>
+                <li>CSS flexbox</li>
+                <li>Jest</li>
+                <li>HOFs</li>
+                <li>APIs</li>
+            </ul>
+          </section>
+        </main>
+    )
+}
+
+export default aboutMe
---
+++ 02-introducao-react/dia-01-iniciando-react/exercise-01/exercise-01/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import aboutMe from './About';
+
+class App extends React.Component {
+  render() {
+    return (aboutMe())
+  }
+}
+
+export default App;
