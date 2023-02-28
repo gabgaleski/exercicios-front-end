ccf195d16e0fcec3e0c3959d38a772a2223c76ad
gabgaleski <galeski35@gmail.com>
2023-02-28 13:52:21 -0300

    Realizando exercicio proposto

--- 02-introducao-react/dia-02-componentes-react/exercise-image-component/src/Image.js
+++ 02-introducao-react/dia-02-componentes-react/exercise-image-component/src/Image.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Image extends React.Component {
   render() {
     const { source, alternativeText } = this.props;
-    return <img src={ source } alt={ alternativeText } />;
+    return <img className='img' src={ source } alt={ alternativeText } />;
   }
 }
 
