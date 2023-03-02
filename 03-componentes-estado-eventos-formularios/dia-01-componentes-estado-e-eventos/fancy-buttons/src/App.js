65eb3cd1204a445faa392be1623c6f7fa3cd4090
gabgaleski <galeski35@gmail.com>
2023-03-02 13:55:10 -0300

    Realizando exercicios de fixação

--- 03-componentes-estado-eventos-formularios/dia-01-componentes-estado-e-eventos/fancy-buttons/src/App.js
+++ 03-componentes-estado-eventos-formularios/dia-01-componentes-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super()
+      this.state = {
+        green: 0,
+        red: 0,
+        blue: 0,
+    }
+  }
+
+  green = () => {
+    this.setState((anterior, _props) => ({
+      green: anterior.green + 1,
+    }))
+  }
+
+  red = () => {
+    this.setState((anterior, _props) => ({
+      red: anterior.red + 1,
+    }))
+  }
+
+  blue = () => {
+    this.setState((stateAnterior, _props) => ({
+      blue: stateAnterior.blue + 1,
+    }))
+  }
+
+  render() {
+    const { green, red, blue } = this.state
+    return ( 
+      <div>
+    <button onClick={this.blue}>Clickes: {blue}</button>
+    <button onClick={this.red}>Clickes: {red}</button>
+    <button onClick={this.green}>Clickes: {green}</button>
+      </div>
+    );
+ }
 }
 
 export default App;
