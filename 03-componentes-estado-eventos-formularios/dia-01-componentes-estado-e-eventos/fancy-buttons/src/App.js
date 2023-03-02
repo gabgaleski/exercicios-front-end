6f0923f9681c4e0ae49f9780497614402fc37488
gabgaleski <galeski35@gmail.com>
2023-03-02 15:19:25 -0300

    Termina exercicios propostos

--- 03-componentes-estado-eventos-formularios/dia-01-componentes-estado-e-eventos/fancy-buttons/src/App.js
+++ 03-componentes-estado-eventos-formularios/dia-01-componentes-estado-e-eventos/fancy-buttons/src/App.js
@@ -13,30 +13,43 @@ class App extends React.Component {
   }
 
   green = () => {
+    const { green } = this.state;
     this.setState((anterior, _props) => ({
       green: anterior.green + 1,
-    }))
+    }), () => {
+      console.log(this.btnColor(green))
+    })
   }
 
   red = () => {
+    const { red } = this.state;
     this.setState((anterior, _props) => ({
       red: anterior.red + 1,
-    }))
+    }), () => {
+      console.log(this.btnColor(red))
+    })
   }
 
   blue = () => {
+    const { blue } = this.state;
     this.setState((stateAnterior, _props) => ({
       blue: stateAnterior.blue + 1,
-    }))
+    }), () => {
+      console.log(this.btnColor(blue))
+    })
+  }
+
+  btnColor = (number) => {
+    return number % 2 === 0 ? 'green' : 'white';
   }
 
   render() {
     const { green, red, blue } = this.state
     return ( 
       <div>
-    <button onClick={this.blue}>Clickes: {blue}</button>
-    <button onClick={this.red}>Clickes: {red}</button>
-    <button onClick={this.green}>Clickes: {green}</button>
+    <button style={{backgroundColor: this.btnColor(blue)}} onClick={this.blue}>Clickes: {blue}</button>
+    <button style={{backgroundColor: this.btnColor(red)}} onClick={this.red}>Clickes: {red}</button>
+    <button style={{backgroundColor: this.btnColor(green)}} onClick={this.green}>Clickes: {green}</button>
       </div>
     );
  }
