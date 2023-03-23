006f399aac0e6a000db1acb74851e77cba7ebbcb
Gabriel Galeski <galeski35@gmail.com>
2023-03-23 16:23:33 -0300

    Realiza alguns testes

--- 06-testes-automatizados-RTL/dia-01-RTL-primeiros-passos/testes-react/src/App.test.js
+++ 06-testes-automatizados-RTL/dia-01-RTL-primeiros-passos/testes-react/src/App.test.js
@@ -1,11 +1,29 @@
+// App.test.js
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Verificando se existe o campo Email.', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputEmail = screen.getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveProperty('type', 'email');
 });
+
+test('Verificando se existe dois botoes', () => {
+  render(<App />);
+  const btn = screen.getAllByRole('button');
+  expect(btn).toHaveLength(2);
+});
+
+test('Verificando se existe um botão', () => {
+  render(<App />);
+  const btn = screen.getByTestId('id-send');
+  expect(btn).toBeInTheDocument();
+  expect(btn).toHaveProperty('type', 'button');
+  expect(btn).toHaveValue('Enviar')
+});
+
 
 // acessar os elementos da tela 
 
