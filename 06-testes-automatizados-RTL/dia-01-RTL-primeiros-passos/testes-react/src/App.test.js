faf8d4197e9ae55eb5bcb1b6043cc489cc4576a1
Gabriel Galeski <galeski35@gmail.com>
2023-03-23 15:46:40 -0300

    Iniciando testes com RTL

---
+++ 06-testes-automatizados-RTL/dia-01-RTL-primeiros-passos/testes-react/src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+// acessar os elementos da tela 
+
+// interagir com os elementos (se for necessario)
+
+// fazer os testes
