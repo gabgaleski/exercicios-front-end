458b21600218d9b5d0777a76bf7e4de9c6215ac9
Gabriel Galeski <galeski35@gmail.com>
2023-03-23 18:35:12 -0300

    Realiza testes

--- 06-testes-automatizados-RTL/dia-01-RTL-primeiros-passos/testes-react/src/App.test.js
+++ 06-testes-automatizados-RTL/dia-01-RTL-primeiros-passos/testes-react/src/App.test.js
@@ -1,28 +1,51 @@
 // App.test.js
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('Verificando se existe o campo Email.', () => {
-  render(<App />);
-  const inputEmail = screen.getByLabelText('Email');
-  expect(inputEmail).toBeInTheDocument();
-  expect(inputEmail).toHaveProperty('type', 'email');
-});
+describe("Tela de inserção de email", () => {
 
-test('Verificando se existe dois botoes', () => {
-  render(<App />);
-  const btn = screen.getAllByRole('button');
-  expect(btn).toHaveLength(2);
-});
+  it('Verificando se existe o campo Email.', () => {
+    render(<App />);
+    const inputEmail = screen.getByLabelText('Email');
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputEmail).toHaveProperty('type', 'email');
+  });
 
-test('Verificando se existe um botão', () => {
-  render(<App />);
-  const btn = screen.getByTestId('id-send');
-  expect(btn).toBeInTheDocument();
-  expect(btn).toHaveProperty('type', 'button');
-  expect(btn).toHaveValue('Enviar')
-});
+ it('Verificando se existe dois botoes', () => {
+   render(<App />);
+   const btn = screen.getAllByRole('button');
+    expect(btn).toHaveLength(2);
+  });
+
+  it('Verificando se existe um botão', () => {
+    render(<App />);
+    const btn = screen.getByTestId('id-send');
+    expect(btn).toBeInTheDocument();
+    expect(btn).toHaveProperty('type', 'button');
+    expect(btn).toHaveValue('Enviar')
+  });
+
+  test('Verificando se o botão e o campo email estão funcionando.', () => {
+    render(<App />);
+  
+    const EMAIL_USER = 'email@email.com';
+  
+    const textEmail = screen.getByTestId('id-email-user');
+    expect(textEmail).toBeInTheDocument();
+    expect(textEmail).toHaveTextContent('Valor:');
+  
+    const btnSend = screen.getByTestId('id-send');
+    const inputEmail = screen.getByLabelText('Email');
+    userEvent.type(inputEmail, EMAIL_USER);
+    userEvent.click(btnSend);
+  
+    expect(inputEmail).toHaveValue('');
+    expect(textEmail).toHaveTextContent(`Valor: ${ EMAIL_USER }`);
+  });
+})
+
 
 
 // acessar os elementos da tela 
