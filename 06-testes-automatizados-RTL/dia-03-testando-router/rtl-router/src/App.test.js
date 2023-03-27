3ea35e6b771b236f9907a6612ad4109c2bfabc49
Gabriel Galeski <galeski35@gmail.com>
2023-03-27 16:08:19 -0300

    Mais testes

--- 06-testes-automatizados-RTL/dia-03-testando-router/rtl-router/src/App.test.js
+++ 06-testes-automatizados-RTL/dia-03-testando-router/rtl-router/src/App.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
-import App from './App';
-
+import App, { About } from './App';
 
 describe('Testando a aplicação', () => {
   it('deve renderizar o componente App', () => {
@@ -18,16 +17,18 @@ describe('Testando a aplicação', () => {
   it('deve renderizar o componente Sobre', async () => {
     const { history } = renderWithRouter(<App />);
   
-    const aboutLink = screen.getByRole('link', { name: 'Sobre' });
+    const aboutLink = screen.getByRole('link', { name: /Sobre/i });
     expect(aboutLink).toBeInTheDocument();
+    
     userEvent.click(aboutLink);
-  
+    const aboutTitle = await screen.findByRole('heading',
+    { name: 'Você está na página Sobre' });
+    expect(aboutTitle).toBeInTheDocument();
+
     const { pathname } = history.location;
     expect(pathname).toBe('/about');
   
-    const aboutTitle = await screen.findByRole('heading',
-      { name: 'Você está na página Sobre' });
-    expect(aboutTitle).toBeInTheDocument();
+    
   });
 
   it('deve testar um caminho não existente e a renderização do Not Found', () => {
@@ -40,5 +41,15 @@ describe('Testando a aplicação', () => {
     const notFoundTitle = screen.getByRole('heading',
       { name: 'Página não encontrada' });
     expect(notFoundTitle).toBeInTheDocument();
+  });
+})
+
+describe('Testando componentes', () => {
+  it('deve renderizar o componente About (apenas componente)', () => {
+    renderWithRouter(<About />);
+  
+    const aboutTitle = screen.getByRole('heading',
+      { name: 'Você está na página Sobre' });
+    expect(aboutTitle).toBeInTheDocument();
   });
 })
