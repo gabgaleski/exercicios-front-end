a2bfdabe05ed5450195eb316de8ab0030752792b
Gabriel Galeski <galeski35@gmail.com>
2023-04-04 13:19:59 -0300

    Utilizando o redux cria o encremento

--- 07-Redux/dia-02-redux-react/my-app/src/redux/index.js
+++ 07-Redux/dia-02-redux-react/my-app/src/redux/index.js
@@ -1,11 +1,7 @@
-// ./src/redux/index.js
 import { legacy_createStore as createStore } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
+import counterReducer from './reducers/index'
 
-const INITIAL_STATE = { count: 0 };
-
-const reducer = (state = INITIAL_STATE, action) => state;
-
-const store = createStore(reducer, composeWithDevTools());
+const store = createStore(counterReducer, composeWithDevTools());
 
 export default store;
